73ad22311f2f2060ab6a6b77fea242331463aa17
Senthilvadivel-20 <senthilsubramaniyam20@gmail.com>
2023-06-13 22:55:55 +0530

    pgsql configured

--- pg_app/settings.py
+++ pg_app/settings.py
@@ -87,25 +87,25 @@ WSGI_APPLICATION = 'pg_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'test',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Welcome123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pg_app',
+        'USER': 'postgres',
+        'PASSWORD': 'Welcome123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 
